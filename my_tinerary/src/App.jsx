587156580df3ce742a78b35a44c5cb4351cf8609
Jonathan Vargas Hurtado <you@example.com>
2024-10-06 18:43:01 -0500

    primer commit

---
+++ my_tinerary/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="text-center">
+      <h1 className="text-4xl font-bold text-blue-600">¡Hola, React con Tailwind!</h1>
+    </div>
+  );
+}
+
+export default App;
+

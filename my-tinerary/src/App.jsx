43f4e7e0da461130eb5d0526fa3a24b2794d6d2a
Jonathan Vargas Hurtado <you@example.com>
2024-10-07 08:10:00 -0500

    segundo commit

---
+++ my-tinerary/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+      <p className="read-the-docs">
+hola mundo      </p>
+      </div>
+
+    </>
+  )
+}
+
+export default App
